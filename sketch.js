fa4c1616074f30bbd081cbc5d4dc33c3d5aba6fc
OjasviSK <80693311+OjasviSK@users.noreply.github.com>
2021-06-14 21:50:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,54 +1,63 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
+var paper, box1, box2, ground, dustbin, dustbinimage;
+var thread;
 
-var cradle1, cradle2, cradle3, cradle4;
-var stand;
+function preload(){
+	dustbinimage=loadImage("dustbingreen.png");
+}
 
-function setup() {
-	createCanvas(800, 700);
+function setup(){
+	createCanvas(800, 400);
 
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
-	stand=new Stand(337,300,250,30);
-	cradle1=new Cradle(250,50,50);
-	cradle2=new Cradle(310,50,50);
-	cradle3=new Cradle(370,50,50);
-	cradle4=new Cradle(430,50,50);
+	dustbin=createSprite(690,320,100,100);
+	dustbin.addImage(dustbinimage);
+	dustbin.scale=0.35;
+	ground=new Box(400,380,900,20);
+	box1=new Box(730,320,5,100);
+	box2=new Box(650,320,5,100);
+	paper=new Ball(100, 300, 50, 50);
 
-	thread1 = new Attach(cradle1.body , stand.body);
-	thread2 = new Attach(cradle2.body , stand.body);
-	thread3 = new Attach(cradle3.body , stand.body);
-	thread4 = new Attach(cradle4.body , stand.body);
-
-}
-
-
-function draw() {
-  background("white");
-  Engine.update(engine);
-  rectMode(CENTER);
- 
-  stand.display();
-  cradle1.display();
-  cradle2.display();
-  cradle3.display();
-  cradle4.display();
-  thread1.display();
-  thread2.display();
-  thread3.display();
-  thread4.display();
+	thread = new Rope(paper.body, {x:200,y:200});
  
 }
 
-function keyPressed(){
+
+function draw(){
+  	background("white");
+  	Engine.update(engine);
+	ground.display();
+	paper.display();
+	box1.display();
+	box2.display();
+	dustbin.display();
+	thread.display();
+	drawSprites();
+}
+
+/*function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(cradle1.body, cradle1.body.position,{x:-0.1,y:-0.01});
+		Matter.Body.applyForce(paper.body, paper.body.position, {x:0, y:-50});
+	}
+	if(keyCode === RIGHT_ARROW){
+		Matter.Body.applyForce(paper.body, paper.body.position, {x:50, y:0});
+	}
+	if(keyCode === LEFT_ARROW){
+		Matter.Body.applyForce(paper.body, paper.body.position, {x:-50, y:0});
 	}
 }
+*/
+function mouseDragged(){
+	Matter.Body.setPosition(paper.body,{x:mouseX,y:mouseY});
+}
 
+function mouseReleased(){
+	thread.fly();
+}
